610dd136252387929fd4bac0c6842e75400d7435
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2019-04-18 09:14:07 +0300

    fetch, async, await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello')
+/* fetch('https://swapi.co/api/people/1/')
+.then((res) => {
+    return res.json();
+})
+.then((body) => {
+    console.log(body)
+}) */
+
+const getResource = async(url) => {
+    const res = await fetch(url);
+
+    if(!res.ok){
+        throw new Error(`Could not fetch ${url}`  + `, received ${res.status}`)
+    }
+    const body = await res.json();
+    return body;
+}
+
+getResource('https://swapi.co/api/people/100000000000000000000000000/')
+    .then((body)=>{
+        console.log(body)
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err)
+    })
